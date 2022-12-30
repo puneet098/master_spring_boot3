11fa6eab37cd3af57dd9e632320eaa6b83642d9a
puneet <puneet098@hotmail.com>
2022-12-31 00:31:15 +0530

    zookeeper commit01

---
+++ learn-spring-framework-zookeeper/src/main/java/com/demo/zookeeper/LearnSpringFrameworkZookeeperApplication.java
@@ -0,0 +1,13 @@
+package com.demo.zookeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringFrameworkZookeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringFrameworkZookeeperApplication.class, args);
+	}
+
+}
---
+++ learn-spring-framework-zookeeper/src/test/java/com/demo/zookeeper/LearnSpringFrameworkZookeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.zookeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringFrameworkZookeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
